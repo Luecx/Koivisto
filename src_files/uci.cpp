c84239aca14f4bd7904bead08f76569b50242e36
Finn Eggers <web@finneggers.de>
2020-06-27 09:30:22 +0200

    uci time update

--- src_files/uci.cpp
+++ src_files/uci.cpp
@@ -6,8 +6,27 @@
 #include "uci.h"
 
 
+#define MONTH (\
+  __DATE__ [2] == 'n' ? (__DATE__ [1] == 'a' ? 1 : 6) \
+: __DATE__ [2] == 'b' ? 2 \
+: __DATE__ [2] == 'r' ? (__DATE__ [0] == 'M' ? 3 : 4) \
+: __DATE__ [2] == 'y' ? 5 \
+: __DATE__ [2] == 'l' ? 7 \
+: __DATE__ [2] == 'g' ? 8 \
+: __DATE__ [2] == 'p' ? 9 \
+: __DATE__ [2] == 't' ? 10 \
+: __DATE__ [2] == 'v' ? 11 \
+: 12)
+#define DAY (\
+    10 * (__DATE__[4] == ' ' ?  '0' : (__DATE__[4] - '0')) + (__DATE__[5] - '0'))
+#define YEAR ((__DATE__[7]-'0') * 1000 + (__DATE__[8]-'0') * 100 + (__DATE__[9]-'0') * 10 + (__DATE__[10]-'0') * 1)
+
+#define BUILD_DATE (char const[]) { __DATE__[0], __DATE__[1], __DATE__[2], __DATE__[3], (__DATE__[4] == ' ' ?  '0' : __DATE__[4]), __DATE__[5], __DATE__[6], __DATE__[7], __DATE__[8], __DATE__[9], __DATE__[10], __DATE__[11] }
+#define BUILD_DATE_NUM (char const[]) {}
+
 Board *board;
 std::thread *searchThread = nullptr;
+
 
 
 void uci_loop(){
@@ -17,7 +36,8 @@ void uci_loop(){
 //    perft_init(false);
     
     
-    std::cout << "Koivisto 64 bit by K. Kahre, F. Eggers" << std::endl;
+    std::cout << "Koivisto 64 "<< YEAR << MONTH << DAY << " by K. Kahre, F. Eggers" << std::endl;
+    
     
     board = new Board();
     
@@ -40,7 +60,7 @@ void uci_loop(){
 }
 
 void uci_uci() {
-    std::cout << "id name Koivisto" << std::endl;
+    std::cout << "id name Koivisto 64 "<< YEAR << MONTH << DAY << std::endl;
     std::cout << "id author K. Kahre, F. Eggers" << std::endl;
     std::cout << "option name Hash type spin default 128 min 1 max " << maxTTSize() <<std::endl;
     
