eedf98e509000d9c4a12e597a2317a05b46fad51
altarchess <kimkahre@outlook.com>
2020-08-17 23:15:30 +0300

    order recaptures before other similar captures
    Score of Koivisto Dev vs Koivisto Dev 2: 620 - 682 - 1232 [0.488]
    Elo difference: -8.50 +/- 9.69
    
    2534 of 5000 games finished.

--- src_files/MoveOrderer.cpp
+++ src_files/MoveOrderer.cpp
@@ -28,7 +28,7 @@ void MoveOrderer::setMovesPVSearch(move::MoveList *moves, move::Move hashMove, S
         }else if(isCapture(m)){
             //add mvv lva score here
             Score SEE = board->staticExchangeEvaluation(m);
-            MoveScore mvvLVA = 10*(getCapturedPiece(m) % 6) - (getMovingPiece(m) % 6);
+            MoveScore mvvLVA = 100 * (getCapturedPiece(m) % 6) - 10 * (getMovingPiece(m) % 6) + (getSquareTo(board->getPreviousMove()) == getSquareTo(m));
             if(SEE >= 0){
                 if(mvvLVA == 0){
                     moves->scoreMove(i, 50000 + mvvLVA);
@@ -37,8 +37,6 @@ void MoveOrderer::setMovesPVSearch(move::MoveList *moves, move::Move hashMove, S
                 }
             }else{
                 MoveScore ms = 8;
-                ms += sd->getHistoryMoveScore(m, board->getActivePlayer());
-                if (ply > 1) ms += sd->getCounterMoveHistoryScore(board->getPreviousMove(), m);
                 moves->scoreMove(i, ms);
             }
         }else if(isPromotion(m)){
@@ -61,14 +59,14 @@ void MoveOrderer::setMovesPVSearch(move::MoveList *moves, move::Move hashMove, S
     
 }
 
-void MoveOrderer::setMovesQSearch(move::MoveList *moves) {
+void MoveOrderer::setMovesQSearch(move::MoveList *moves, Board *b) {
     this->moves = moves;
     this->counter = 0;
 
     for(int i = 0; i < moves->getSize(); i++){
         move::Move m = moves->getMove(i);
 
-        MoveScore mvvLVA = 10*(getCapturedPiece(m) % 6) - (getMovingPiece(m) % 6);
+        MoveScore mvvLVA = 100*(getCapturedPiece(m) % 6) - 10*(getMovingPiece(m) % 6)+(getSquareTo(b->getPreviousMove()) == getSquareTo(m));
         moves->scoreMove(i, 240 + mvvLVA);
     }
 }
