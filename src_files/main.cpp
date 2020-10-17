84b20b00a8faac5149a365a508ed3c7bd3302ecb
Finn Eggers <web@finneggers.de>
2020-10-17 20:15:40 +0200

    fixed main

--- src_files/main.cpp
+++ src_files/main.cpp
@@ -94,6 +94,7 @@ void main_tune_pst_bb(Piece piece) {
 }
 
 void main_tune_features() {
+    eval_init();
     bb_init();
     Evaluator* evaluator = new Evaluator();
 
@@ -106,33 +107,32 @@ void main_tune_features() {
 
         std::cout << "--------------------------------------------------- [" << i
                   << "] ----------------------------------------------" << std::endl;
-//        std::cout << std::setprecision(8) << tuning::optimiseGD(evaluator, K, 1e4) << std::endl;
     
         std::cout <<tuning::optimisePSTBlackBox(evaluator, K, pieceScores, 6, 1)<< std::endl;
+        std::cout << tuning::optimisePSTBlackBox(evaluator, K, evfeatures, 20, 1) << std::endl;
+        std::cout <<tuning::optimisePSTBlackBox(evaluator, K, pinnedEval, 15, 1)<< std::endl;
+        std::cout <<tuning::optimisePSTBlackBox(evaluator, K, hangingEval, 5, 1)<< std::endl;
 
-//        std::cout <<tuning::optimisePSTBlackBox(evaluator, K, pinnedEval, 15, 1)<< std::endl;
-//        std::cout <<tuning::optimisePSTBlackBox(evaluator, K, hangingEval, 5, 1)<< std::endl;
-    
         for (Square s = 0; s < 6; s++) {
             std::cout << "M(" << setw(5) << MgScore(pieceScores[s]) << "," << setw(5) << EgScore(pieceScores[s]) << "), ";
             std::cout << std::endl;
         }
         std::cout << std::endl;
-//        for (Square s = 0; s < 20; s++) {
-//            std::cout << "M(" << setw(5) << MgScore(*features[s]) << "," << setw(5) << EgScore(*features[s]) << "), ";
-//            std::cout << std::endl;
-//        }
-//        std::cout << std::endl;
-//
-//        for (Square s = 0; s < 15; s++) {
-//            std::cout << "M(" << setw(5) << MgScore(pinnedEval[s]) << "," << setw(5) << EgScore(pinnedEval[s]) << "), ";
-//            std::cout << std::endl;
-//        }
-//        std::cout << std::endl;
-//        for (Square s = 0; s < 5; s++) {
-//            std::cout << "M(" << setw(5) << MgScore(hangingEval[s]) << "," << setw(5) << EgScore(hangingEval[s]) << "), ";
-//            std::cout << std::endl;
-//        }
+        for (Square s = 0; s < 20; s++) {
+            std::cout << "M(" << setw(5) << MgScore(*evfeatures[s]) << "," << setw(5) << EgScore(*evfeatures[s]) << "), ";
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+
+        for (Square s = 0; s < 15; s++) {
+            std::cout << "M(" << setw(5) << MgScore(pinnedEval[s]) << "," << setw(5) << EgScore(pinnedEval[s]) << "), ";
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        for (Square s = 0; s < 5; s++) {
+            std::cout << "M(" << setw(5) << MgScore(hangingEval[s]) << "," << setw(5) << EgScore(hangingEval[s]) << "), ";
+            std::cout << std::endl;
+        }
         std::cout << std::endl;
     }
 
@@ -178,6 +178,7 @@ void main_tune_pst() {
 #endif
 
 int main(int argc, char* argv[]) {
+
     
     if (argc == 1) {
         uci_loop(false);
