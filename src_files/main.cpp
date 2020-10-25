cbf7c8bc304c0fada67e81b5bcef2151caa78728
altarchess <altarchess@users.noreply.github.com>
2020-10-25 13:46:23 +0200

    tuned

--- src_files/main.cpp
+++ src_files/main.cpp
@@ -52,31 +52,73 @@ void main_tune_pst_bb(Piece piece) {
 
     using namespace tuning;
 
+
     loadPositionFile("../resources/other/tuningset_clear2.epd", 2500000);
+
+    eval_init();
     auto K = tuning::computeK(evaluator, 2.86681, 200, 1e-7);
 
-    int lr = 10;
+    int lr = 200;
 
     for (int i = 1; i < 5000; i++) {
 
         std::cout << "--------------------------------------------------- [" << i
                   << "] ----------------------------------------------" << std::endl;
     
-        std::cout <<tuning::optimisePSTBlackBox(evaluator, K, psqt[0], 64, lr)<< std::endl;
-        std::cout << tuning::optimisePSTBlackBox(evaluator, K, psqt[1], 64, lr) << std::endl;
+        std::cout <<tuning::optimisePSTBlackBox(evaluator, K, psqt[2], 64, lr)<< std::endl;
+        std::cout << tuning::optimisePSTBlackBox(evaluator, K, psqt[3], 64, lr) << std::endl;
+
+        std::cout <<tuning::optimisePSTBlackBox(evaluator, K, psqt[4], 64, lr)<< std::endl;
+        std::cout << tuning::optimisePSTBlackBox(evaluator, K, psqt[5], 64, lr) << std::endl;
+
+        std::cout <<tuning::optimisePSTBlackBox(evaluator, K, psqt[6], 64, lr)<< std::endl;
+        std::cout << tuning::optimisePSTBlackBox(evaluator, K, psqt[7], 64, lr) << std::endl;
+
+        std::cout <<tuning::optimisePSTBlackBox(evaluator, K, psqt[8], 64, lr)<< std::endl;
+        std::cout << tuning::optimisePSTBlackBox(evaluator, K, psqt[9], 64, lr) << std::endl;
 
         for (Square s = 0; s < 64; s++) {
-            std::cout << "M(" << setw(5) << MgScore(psqt[0][s]) << "," << setw(5) << EgScore(psqt[0][s]) << "), ";
+            std::cout << "M(" << setw(5) << MgScore(psqt[2][s]) << "," << setw(5) << EgScore(psqt[2][s]) << "), ";
             if ((i+1)%8==0)std::cout << std::endl;
         }
         std::cout << std::endl;
         for (Square s = 0; s < 64; s++) {
-            std::cout << "M(" << setw(5) << MgScore(psqt[1][s]) << "," << setw(5) << EgScore(psqt[1][s]) << "), ";
+            std::cout << "M(" << setw(5) << MgScore(psqt[3][s]) << "," << setw(5) << EgScore(psqt[3][s]) << "), ";
+            if ((i+1)%8==0)std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        for (Square s = 0; s < 64; s++) {
+            std::cout << "M(" << setw(5) << MgScore(psqt[4][s]) << "," << setw(5) << EgScore(psqt[4][s]) << "), ";
+            if ((i+1)%8==0)std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        for (Square s = 0; s < 64; s++) {
+            std::cout << "M(" << setw(5) << MgScore(psqt[5][s]) << "," << setw(5) << EgScore(psqt[5][s]) << "), ";
+            if ((i+1)%8==0)std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        for (Square s = 0; s < 64; s++) {
+            std::cout << "M(" << setw(5) << MgScore(psqt[6][s]) << "," << setw(5) << EgScore(psqt[6][s]) << "), ";
+            if ((i+1)%8==0)std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        for (Square s = 0; s < 64; s++) {
+            std::cout << "M(" << setw(5) << MgScore(psqt[7][s]) << "," << setw(5) << EgScore(psqt[7][s]) << "), ";
+            if ((i+1)%8==0)std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        for (Square s = 0; s < 64; s++) {
+            std::cout << "M(" << setw(5) << MgScore(psqt[8][s]) << "," << setw(5) << EgScore(psqt[8][s]) << "), ";
+            if ((i+1)%8==0)std::cout << std::endl;
+        }
+        std::cout << std::endl;
+        for (Square s = 0; s < 64; s++) {
+            std::cout << "M(" << setw(5) << MgScore(psqt[9][s]) << "," << setw(5) << EgScore(psqt[9][s]) << "), ";
             if ((i+1)%8==0)std::cout << std::endl;
         }
         std::cout << std::endl;
 
-        if (i%10 == 0 && lr>1){
+        if (i%3 == 0 && lr>1){
             lr /= 2;
             std::cout << "Lr Dropped to " << lr;
             std::cout << std::endl;
@@ -175,11 +217,11 @@ void main_tune_pst() {
 int main(int argc, char* argv[]) {
 
     
-   /*if (argc == 1) {
+   if (argc == 1) {
         uci_loop(false);
     } else if (argc > 1 && strcmp(argv[1], "bench") == 0) {
         uci_loop(true);
-    }*/
+    }
  
     
     
@@ -195,7 +237,7 @@ int main(int argc, char* argv[]) {
 //     tuning::evalSpeed();
 //     bb_cleanUp();
 
-        main_tune_pst_bb(PAWN);
+       // main_tune_pst_bb(PAWN);
 //    eval_init();
 //    main_tune_features();
     // main_tune_pst();
