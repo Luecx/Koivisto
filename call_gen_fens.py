38e41626bb8647352e3e1894d6316c7f9ff9e32f
Finn Eggers <web@finneggers.de>
2020-11-02 21:58:03 +0100

    fen generation

--- call_gen_fens.py
+++ call_gen_fens.py
@@ -1,14 +1,8 @@
 import subprocess
-import psutil
 import time
 import os
 import sys
 
-# Aggressively kill stray processes we're not correctly cleaning up.
-def killall():
-    for proc in psutil.process_iter():
-        if "koivisto" in proc.name().lower():
-            proc.kill()
 
 print(f"Launches `num_proc` koivistos in parallel.")
 print(f"Usage: python3 {sys.argv[0]} <num_proc> <args>")
@@ -20,8 +14,8 @@ print()
 print()
 
 N_PROC = int(sys.argv[1])
+path = [x for x in sys.argv if "__proc_idx__" in x][-1]
 while True:
-    killall()
 
     procs = []
     for i in range(N_PROC):
@@ -29,10 +23,8 @@ while True:
         procs.append(subprocess.Popen([x.replace("__proc_idx__", str(i)) for x in sys.argv[2:]], stdout = subprocess.DEVNULL))
         time.sleep(0.1)
 
-    os.system("cat positions.*.txt | wc -l > n_pos")
-    os.system("cat evals.*.txt | wc -l > n_evs")
+    os.system(f"cat {path.replace('__proc_idx__','*')} | wc -l > n_pos")
     npos = int(open("n_pos").read().strip())
-    nevs = int(open("n_evs").read().strip())
     t = time.time()
     for i in range(3600):
         while True:
@@ -40,14 +32,9 @@ while True:
             if time.time() - t > 20:
                 break
 
-        os.system("cat positions.*.txt | wc -l > n_pos")
-        os.system("cat evals.*.txt | wc -l > n_evs")
+        os.system(f"cat {path.replace('__proc_idx__','*')} | wc -l > n_pos")
         new_npos = int(open("n_pos").read().strip())
-        new_nevs = int(open("n_evs").read().strip())
         pps = (new_npos - npos) / (time.time() - t)
-        eps = (new_nevs - nevs) / (time.time() - t)
         npos = new_npos
-        nevs = new_nevs
         print("TOTAL POSITIONS: {:.3f}M, {}pps".format(npos / 1000 / 1000, int(pps)))
-        print("TOTAL EVALS:     {:.3f}M, {}eps".format(nevs / 1000 / 1000, int(eps)))
         t = time.time()
