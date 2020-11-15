e8571174bf366aa685aafbce0d3cb4c646319045
Finn Eggers <web@finneggers.de>
2020-11-15 15:50:53 +0100

    fixed main

--- src_files/main.cpp
+++ src_files/main.cpp
@@ -95,7 +95,7 @@ void main_tune_features() {
 int main(int argc, char* argv[]) {
 
     if (argc == 1) {
-        uci_loop(true);
+        uci_loop(false);
     } else if (argc > 1 && strcmp(argv[1], "bench") == 0) {
         uci_loop(true);
     }
