15339ef524255e2ed24beb8fb34d24c7d7cb2654
Kim Kåhre <kim.kahre@helsinki.fi>
2020-11-21 23:45:42 +0200

    seecache

--- src_files/search.h
+++ src_files/search.h
@@ -44,7 +44,7 @@ extern int FUTILITY_MARGIN;
 extern int SE_MARGIN_STATIC;
 extern int LMR_DIV;
 
-#define MAX_THREADS 32
+#define MAX_THREADS 256
 
 /**
  * used to store information about a search
