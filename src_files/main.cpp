6608e622bd9e2b9210a8f97e5d56bd0bedcb40ef
Eugenio Bruno <brunoeugeniop@gmail.com>
2020-12-07 18:29:26 +0100

    Tried tuning queens
    "Just pull, copy from stockfish, and paste"
       -- Finn, 2020
    
    (editor's note: that is not what Finn actually said, this is a joke)

--- src_files/main.cpp
+++ src_files/main.cpp
@@ -40,9 +40,9 @@ void main_tune_features() {
     using namespace tuning;
 
     //    loadPositionFile("../resources/other/quiet-labeled2.epd", 100000);
-    loadPositionFile("../resources/other/E12.33-1M-D12-Resolved.book", 10000000);
-    loadPositionFile("../resources/other/E12.41-1M-D12-Resolved.book", 10000000);
-    loadPositionFile("../resources/other/E12.46FRC-1250k-D12-1s-Resolved.book", 10000000);
+    loadPositionFile("E:/a.book", 10000000);
+    loadPositionFile("E:/b.book", 10000000);
+    loadPositionFile("E:/c.book", 10000000);
     auto K = tuning::computeK(2.86681, 200, 1e-7, 16);
 
     for (int i = 0; i < 100; i++) {
@@ -54,7 +54,7 @@ void main_tune_features() {
         //        tuning::optimisePSTBlackBox(K, reinterpret_cast<EvalScore*>(piece_square_table[0][1]),64,10,3,16);
         //        tuning::optimisePSTBlackBox(K,
         //        reinterpret_cast<EvalScore*>(piece_our_king_square_table[0]),225,10,3,16);
-        tuning::optimisePSTBlackBox(K, reinterpret_cast<EvalScore*>(piece_opp_king_square_table[0]), 225, 100, 3, 16);
+        tuning::optimisePSTBlackBox(K, reinterpret_cast<EvalScore*>(piece_opp_king_square_table[4]), 225, 100, 3, 16);
 
         //        for (int s = 0; s < 64; s++) {
         //            if (s%8 == 0) std::cout << std::endl;
@@ -78,8 +78,8 @@ void main_tune_features() {
         for (int s = 0; s < 225; s++) {
             if (s % 15 == 0)
                 std::cout << std::endl;
-            std::cout << "M(" << setw(5) << MgScore(piece_opp_king_square_table[0][s]) << "," << setw(5)
-                      << EgScore(piece_opp_king_square_table[0][s]) << "), ";
+            std::cout << "M(" << setw(5) << MgScore(piece_opp_king_square_table[4][s]) << "," << setw(5)
+                      << EgScore(piece_opp_king_square_table[4][s]) << "), ";
         }
         std::cout << std::endl;
         //        std::cout << std::endl;
@@ -123,7 +123,7 @@ int main(int argc, char* argv[]) {
 
     // main_tune_pst_bb(PAWN);
     //    psqt_init();
-    //     main_tune_features();
+    // main_tune_features();
     // main_tune_pst();
     // main_tune_features_bb();
 
