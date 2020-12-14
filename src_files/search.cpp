57aea88a5b7a6937368362ed445ac773b3af6613
Kim Kåhre <kim.kahre@helsinki.fi>
2020-12-14 03:26:07 +0200

    test
    bench: 7937969

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -788,7 +788,7 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
                     moveOrderer.skip = true;
                     continue;
                 }
-                if (sd->getHistories(m, b->getActivePlayer(), b->getPreviousMove()) < 200-30*(depth*depth)){
+                if (!sd->isKiller(m, ply, b->getActivePlayer()) &&  sd->getHistories(m, b->getActivePlayer(), b->getPreviousMove()) < 200-30*(depth*depth)){
                     continue;
                 }
             }
