fd1325ede474278087167fcd135a27308c3faa73
Finn Eggers <web@finneggers.de>
2020-12-17 20:17:46 +0100

    limit nmp by depth
    bench: 8765087

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -707,9 +707,10 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
         // if the evaluation from a very shallow search after doing nothing is still above beta, we assume that we are
         // currently above beta as well and stop the search early.
         // **********************************************************************************************************
-        if (staticEval >= beta && !hasOnlyPawns(b, b->getActivePlayer())) {
+        if (staticEval >= beta && !hasOnlyPawns(b, b->getActivePlayer()) && depth > 3) {
             b->move_null();
-            score = -pvSearch(b, -beta, 1 - beta, depth - (depth / 4 + 3) * ONE_PLY - (staticEval-beta<300 ? (staticEval-beta)/FUTILITY_MARGIN : 3), ply + ONE_PLY, td, 0);
+            score = -pvSearch(b, -beta, 1 - beta, depth - (depth / 4 + 3) * ONE_PLY -
+                         (staticEval-beta<300 ? (staticEval-beta)/FUTILITY_MARGIN : 3), ply + ONE_PLY, td, 0);
             b->undoMove_null();
             if (score >= beta) {
                 return beta;
