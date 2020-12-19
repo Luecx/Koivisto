9f63b73e61e32085746038cf751c66f0c93ba1f3
Kim Kahre <kimkahre@outlook.com>
2020-12-19 03:02:25 +0200

    test3
    bench: 7411872

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -786,7 +786,7 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
                 // late move pruning:
                 // if the depth is small enough and we searched enough quiet moves, dont consider this move
                 // **************************************************************************************************
-                if (depth <= 14 && legalMoves >= lmp[isImproving][depth]) {
+                if (depth <= 14 && quiets > lmp[isImproving][depth]) {
                     moveOrderer.skip = true;
                     continue;
                 }
