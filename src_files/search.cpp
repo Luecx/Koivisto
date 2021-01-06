12b30544fd9ca7fd848595bf20905546e6af1d5b
Kim Kahre <kimkahre@outlook.com>
2021-01-06 23:08:16 +0200

    test
    bench: 7957901

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -852,13 +852,16 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
         // we dont want to reduce if its the first move we search, or a capture with a positive see score or if the
         // depth is too small.
         // furthermore no queen promotions are reduced
-        Depth lmr = (legalMoves == 0 || depth <= 2 || (isCapture(m) && staticExchangeEval >= 0)
-                     || (isPromotion && (promotionPiece(m) % 6 == QUEEN)))
-                    ? 0
-                    : lmrReductions[depth][legalMoves];
+        Depth lmr;
         
         // depending on if lmr is used, we adjust the lmr score using history scores and kk-reductions.
-        if (lmr) {
+        if (legalMoves < 1+(ply == 0) || depth <= 2 || (isCapture(m) && staticExchangeEval >= 0)
+                     || (isPromotion && (promotionPiece(m) % 6 == QUEEN))) 
+        {
+            lmr = 0;
+        }
+        else {
+            lmr = lmrReductions[depth][legalMoves];
             int history = 0;
             lmr = lmr - sd->getHistories(m, b->getActivePlayer(), b->getPreviousMove()) / 256;
             lmr += !isImproving;
