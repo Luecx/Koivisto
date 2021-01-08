20f7529c712b6987a57324f4471134b53aeba24b
Finn Eggers <web@finneggers.de>
2021-01-08 11:47:43 +0100

    works now

--- src_files/main.cpp
+++ src_files/main.cpp
@@ -127,9 +127,9 @@ int main(int argc, char* argv[]) {
     load_positions("../resources/E12.33-1M-D12-Resolved.book", 10000000 ,0 );
     load_positions("../resources/E12.41-1M-D12-Resolved.book", 10000000 ,0 );
     load_positions("../resources/E12.46FRD-1250k-D12-1s-Resolved.book", 10000000 ,0 );
-    compute_K(3, 100, 1e-7);
+    compute_K(2.48617, 100, 1e-7);
     for(int i = 0; i < 100; i++){
-        train(100, 2.48172, 10000);
+        train(100, 2.48172, 60000);
         display_params();
     }
 
