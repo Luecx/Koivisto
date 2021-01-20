f8a32f1e1cfd0eac0c1bbf7acfe54f6f0071a5e5
Kim Kåhre <kim.kahre@helsinki.fi>
2021-01-20 11:43:16 +0200

    test
    bench: 6186982

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -699,7 +699,7 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
         // if the static evaluation is already above beta with a specific margin, assume that the we will definetly be
         // above beta and stop the search here and fail soft
         // **********************************************************************************************************
-        if (depth <= 7 && staticEval >= beta + depth * FUTILITY_MARGIN && staticEval < MIN_MATE_SCORE)
+        if (depth <= 7 && staticEval >= beta + (depth-isImproving) * FUTILITY_MARGIN && staticEval < MIN_MATE_SCORE)
             return staticEval;
         
         // **********************************************************************************************************
