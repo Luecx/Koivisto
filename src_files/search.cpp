c78717db43b194a989f18d800be79e619471ece8
Kim Kåhre <kim.kahre@helsinki.fi>
2021-01-21 00:51:59 +0200

    expand
    bench: 7602398

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -869,9 +869,8 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
             lmr = lmr - sd->getHistories(m, b->getActivePlayer(), b->getPreviousMove()) / 150;
             lmr += !isImproving;
             lmr -= pv;
-            if (sd->reduce && sd->sideToReduce != b->getActivePlayer()) {
-                lmr = lmr + 1;
-            }
+            if (sd->isKiller(m, ply, b->getActivePlayer())) lmr--;
+            if (sd->reduce && sd->sideToReduce != b->getActivePlayer()) lmr++;
             if (lmr > MAX_PLY) {
                 lmr = 0;
             }
@@ -924,7 +923,8 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
                 table->put(zobrist, score, m, CUT_NODE, depth);
             }
             // also set this move as a killer move into the history
-            sd->setKiller(m, ply, b->getActivePlayer());
+            if (!isCapture(m))
+                sd->setKiller(m, ply, b->getActivePlayer());
             // if the move is not a capture, we also update counter move history tables and history scores.
             
             sd->updateHistories(m, depth, mv, b->getActivePlayer(), b->getPreviousMove());
