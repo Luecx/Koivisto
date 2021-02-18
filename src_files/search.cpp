6b074ceb0f0085b9f267a114b2b9689102e874e2
Kim Kåhre <kim.kahre@helsinki.fi>
2021-02-18 17:39:52 +0200

    test
    bench: 7728712

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -850,7 +850,7 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
         // kk reductions:
         // we reduce more/less depending on which side we are currently looking at.
         // *********************************************************************************************************
-        if (pv) {
+        if (ply == 0) {
             sd->sideToReduce = !b->getActivePlayer();
             sd->reduce = false;
             if (legalMoves == 0){
@@ -896,7 +896,7 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
             score = -pvSearch(b, -beta, -alpha, depth - ONE_PLY + extension, ply + ONE_PLY, td, 0);
         } else {
             score = -pvSearch(b, -alpha - 1, -alpha, depth - ONE_PLY - lmr + extension, ply + ONE_PLY, td, 0, &lmr);
-            if (pv) sd->reduce = true;
+            if (ply == 0) sd->reduce = true;
             if (lmr && score > alpha)
                 score = -pvSearch(b, -alpha - 1, -alpha, depth - ONE_PLY + extension, ply + ONE_PLY, td,
                                   0);    // re-search
