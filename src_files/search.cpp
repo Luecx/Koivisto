a2f8c997419de380bef7754ef280fe7ea14db91a
Kim Kåhre <kim.kahre@helsinki.fi>
2021-04-09 13:23:18 +0300

    test
    bench: 9610513

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -915,6 +915,7 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
             lmr = lmr - sd->getHistories(m, b->getActivePlayer(), b->getPreviousMove()) / 150;
             lmr += !isImproving;
             lmr -= pv;
+            lmr -= ply == 0;
             if (sd->isKiller(m, ply, b->getActivePlayer())) lmr--;
             if (sd->reduce && sd->sideToReduce != b->getActivePlayer()) lmr++;
             if (lmr > MAX_PLY) {
