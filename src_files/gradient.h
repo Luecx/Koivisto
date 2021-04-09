59815aa1beaecb602a4dd450e9368706e6f17734
Finn Eggers <web@finneggers.de>
2021-04-09 23:07:59 +0200

    go_nodes

--- src_files/gradient.h
+++ src_files/gradient.h
@@ -1,6 +1,21 @@
-//
-// Created by finne on 06.01.2021.
-//
+
+/****************************************************************************************************
+ *                                                                                                  *
+ *                                     Koivisto UCI Chess engine                                    *
+ *                                   by. Kim Kahre and Finn Eggers                                  *
+ *                                                                                                  *
+ *                 Koivisto is free software: you can redistribute it and/or modify                 *
+ *               it under the terms of the GNU General Public License as published by               *
+ *                 the Free Software Foundation, either version 3 of the License, or                *
+ *                                (at your option) any later version.                               *
+ *                    Koivisto is distributed in the hope that it will be useful,                   *
+ *                  but WITHOUT ANY WARRANTY; without even the implied warranty of                  *
+ *                   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  *
+ *                           GNU General Public License for more details.                           *
+ *                 You should have received a copy of the GNU General Public License                *
+ *                 along with Koivisto.  If not, see <http://www.gnu.org/licenses/>.                *
+ *                                                                                                  *
+ ****************************************************************************************************/
 
 #ifndef KOIVISTO_GRADIENT_H
 #define KOIVISTO_GRADIENT_H
