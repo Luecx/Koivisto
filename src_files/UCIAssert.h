e5b448064d91c3d2cb4cfc077c66754be21a13b8
kz04px <a>
2021-04-10 17:01:59 +0100

    Make UCI_ASSERT friendly with other compilers
    Not everyone likes __PRETTY_FUNCTION__
    Did not test _MSC_VER

--- src_files/UCIAssert.h
+++ src_files/UCIAssert.h
@@ -3,6 +3,14 @@
 
 #include <iostream>
 
+#if defined(__GNUC__) || defined(__clang__)
+    #define FUNCTION_NAME __PRETTY_FUNCTION__
+#elif _MSC_VER
+    #define FUNCTION_NAME __FUNCSIG__
+#else
+    #define FUNCTION_NAME __func__
+#endif
+
 #ifdef NDEBUG
 #define UCI_ASSERT(expr)
 #else
@@ -13,7 +21,7 @@
             std::cout << "info string expression " << (#expr) << std::endl;                                            \
             std::cout << "info string file " << __FILE__ << std::endl;                                                 \
             std::cout << "info string line " << __LINE__ << std::endl;                                                 \
-            std::cout << "info string function " << __PRETTY_FUNCTION__ << std::endl;                                  \
+            std::cout << "info string function " << FUNCTION_NAME << std::endl;                                        \
             std::exit(1);                                                                                              \
         }                                                                                                              \
     }
