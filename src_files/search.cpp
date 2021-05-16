9bb2d4492fb3f5fa386fd0cbe810aed92db0da9a
Kim Kåhre <kim.kahre@helsinki.fi>
2021-05-16 18:24:39 +0300

    test
    bench: 8587166

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -889,7 +889,7 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
         // standard implementation apart from the fact that we cancel lmr of parent node in-case the node turns 
         // out to be singular.
         // *********************************************************************************************************
-        if (!extension && depth >= 8 && !skipMove && legalMoves == 0 && sameMove(m, hashMove) && ply > 0
+        if (depth >= 8 && !skipMove && legalMoves == 0 && sameMove(m, hashMove) && ply > 0
             && en.zobrist == zobrist && abs(en.score) < MIN_MATE_SCORE
             && (en.type == CUT_NODE || en.type == PV_NODE) && en.depth >= depth - 3) {
 
@@ -935,11 +935,9 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
                     ? 0
                     : lmrReductions[depth][legalMoves];
         
-        if (b->getActivePlayer() == behindLmr && legalMoves > 0 && depth > 2) lmr++;
 
         // depending on if lmr is used, we adjust the lmr score using history scores and kk-reductions.
         if (lmr) {
-            int history = 0;
             lmr = lmr - sd->getHistories(m, b->getActivePlayer(), b->getPreviousMove()) / 150;
             lmr += !isImproving;
             lmr -= pv;
@@ -967,7 +965,7 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
         } else {
             score = -pvSearch(b, -alpha - 1, -alpha, depth - ONE_PLY - lmr + extension, ply + ONE_PLY, td, 0, lmr ? b->getActivePlayer() : behindLmr,  &lmr);
             if (pv) sd->reduce = true;
-            if (lmr && score > alpha)
+            if (lmr && score > alpha && (b->getActivePlayer() != behindLmr))
                 score = -pvSearch(b, -alpha - 1, -alpha, depth - ONE_PLY + extension, ply + ONE_PLY, td,
                                   0, behindLmr);    // re-search
             if (score > alpha && score < beta)
