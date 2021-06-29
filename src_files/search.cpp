2d9b3e67dd7935e53e717bb42ae6c9c015bfbc2f
Kim Kåhre <kim.kahre@helsinki.fi>
2021-06-29 20:18:25 +0300

    split quiet and noisy see
    bench: 8254281

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -914,6 +914,9 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
                 if (sd->getHistories(m, b->getActivePlayer(), b->getPreviousMove()) < std::min(200-30*(depth*depth), 0)){
                     continue;
                 }
+                if (moveDepth <= 5 && (getCapturedPieceType(m)) < (getMovingPieceType(m))
+                    && b->staticExchangeEvaluation(m) <= -40*moveDepth)
+                    continue;
             }
             
             // ******************************************************************************************************
@@ -921,9 +924,10 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
             // if the depth we are going to search the move at is small enough and the static exchange evaluation for the given move is very negative, dont
             // consider this quiet move as well.
             // ******************************************************************************************************
-            if (moveDepth <= 5 && (getCapturedPieceType(m)) < (getMovingPieceType(m))
-                && b->staticExchangeEvaluation(m) <= (quiet ? -40*moveDepth : -100 * depth))
+            if ((getCapturedPieceType(m)) < (getMovingPieceType(m)) && b->staticExchangeEvaluation(m) <= -80 * depth)
                 continue;
+
+            
         }
         
         // dont search illegal moves
