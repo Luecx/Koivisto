ffe9ac0cecbc2653b689d8c452edff48c6692f16
Kim Kåhre <kim.kahre@helsinki.fi>
2021-06-30 16:39:35 +0300

    test
    bench: 6727055

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -556,7 +556,7 @@ Move bestMove(Board* b, Depth maxDepth, TimeManager* timeManager, int threadId) 
         if (d < 6) {
             s = pvSearch(&searchBoard, -MAX_MATE_SCORE, MAX_MATE_SCORE, d, 0, td, 0, 2);
         } else {
-            Score window = 10;
+            Score window = 12;
             Score alpha  = s - window;
             Score beta   = s + window;
             
