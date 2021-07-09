87bf3694916e10cbae662db7f14fa4f4deacb120
Kim Kåhre <kim.kahre@helsinki.fi>
2021-07-09 16:19:03 +0300

    test
    bench: 6547007

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -1323,7 +1323,7 @@ Score qSearch(Board* b, Score alpha, Score beta, Depth ply, ThreadData* td, bool
 
         // if the move seems to be really good just return beta.
         if (+see_piece_vals[(getPieceType(getCapturedPiece(m)))]
-                - see_piece_vals[getPieceType(getMovingPiece(m))] - 300 + stand_pat
+                - see_piece_vals[getPieceType(getMovingPiece(m))] - 330 + stand_pat
             > beta)
             return beta;
 
