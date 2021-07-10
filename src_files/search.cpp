f37336feb7babbf87a0b07c01ebef5ad7106eb24
Kim Kåhre <kim.kahre@helsinki.fi>
2021-07-10 09:46:55 +0300

    test
    bench: 6778346

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -978,7 +978,7 @@ Score pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply, Thread
 
         if (ply > 0 && legalMoves >= 1 && highestScore > -MIN_MATE_SCORE) {
 
-            Depth moveDepth = std::max(1, depth - lmrReductions[depth][legalMoves]);
+            Depth moveDepth = std::max(1, depth - std::min((int)depth, lmrReductions[depth][legalMoves]));
 
             if (quiet) {
                 quiets++;
