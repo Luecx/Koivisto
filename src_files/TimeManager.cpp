5032c77edf222e79ab7f903d0257b8b207ddc424
Kim Kåhre <kim.kahre@helsinki.fi>
2021-07-13 01:37:26 +0300

    test
    bench: 7060969

--- src_files/TimeManager.cpp
+++ src_files/TimeManager.cpp
@@ -154,7 +154,7 @@ bool TimeManager::rootTimeLeft(int score) {
         return false;
 
     // if we are above the maximum allowed time at root, stop
-    if (elapsed >= timeToUse*50/std::max(score, 40))
+    if (elapsed >= timeToUse*50/std::max(score, 20))
         return false;
 
     return true;
