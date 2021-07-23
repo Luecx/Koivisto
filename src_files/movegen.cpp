c4ed478ceb60d459396d9ac70d80b917416533dc
Kim Kåhre <kim.kahre@helsinki.fi>
2021-07-23 12:59:25 +0300

    test
    bench: 6121292

--- src_files/movegen.cpp
+++ src_files/movegen.cpp
@@ -55,7 +55,7 @@ inline void scoreMove(Board* board, MoveList* mv, Move hashMove, SearchData* sd,
             if (SEE >= 0) {
                 mv->scoreMove(idx, 100000 + mvvLVA + sd->getHistories(move, board->getActivePlayer(), board->getPreviousMove()));
             } else {
-                mv->scoreMove(idx, 10000 + sd->getHistories(move, board->getActivePlayer(), board->getPreviousMove()));
+                mv->scoreMove(idx, 10000 - piece_values[(getMovingPieceType(move))] + sd->getHistories(move, board->getActivePlayer(), board->getPreviousMove()));
             }
         } else if constexpr (isPromotion){
             MoveScore mvvLVA = (getCapturedPieceType(move)) - (getMovingPieceType(move));
