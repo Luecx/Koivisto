3ccef3684b1b7a921fc2f03813f4d69f5923ada3
Kim Kåhre <kim.kahre@helsinki.fi>
2021-07-23 16:17:25 +0300

    test
    bench: 4516662

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -464,7 +464,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
         // if the static evaluation is already above beta at depth 1 and we have strong threats, asume
         // that we can atleast achieve beta
         // **********************************************************************************************************
-        if (depth == 1 && staticEval > beta + 30 && ownThreats && !enemyThreats)
+        if (depth == 1 && staticEval > beta + 30 && !enemyThreats)
             return beta;
 
         // **********************************************************************************************************
