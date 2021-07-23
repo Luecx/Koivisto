7e732c5524bd9a30ac7d64b3ccd403394e48db77
Finn Eggers <web@finneggers.de>
2021-07-23 15:20:21 +0200

    kimmy messed up as usual

--- src_files/TimeManager.cpp
+++ src_files/TimeManager.cpp
@@ -152,9 +152,14 @@ bool TimeManager::rootTimeLeft(int score) {
     // stop the search if requested
     if (forceStop)
         return false;
-
+    
+    // dont use any correction if not in tournament mode
+    if (mode != TOURNAMENT && elapsed >= timeToUse){
+        return false;
+    }
+    
     // if we are above the maximum allowed time at root, stop
-    if (elapsed >= timeToUse*50/std::max(score, 30))
+    if (mode == TOURNAMENT && elapsed >= timeToUse * 50/std::max(score, 30))
         return false;
 
     return true;
