6cdb8ab995da41554d716b0ce32550fa81997467
Aryan1508 <68417405+Aryan1508@users.noreply.github.com>
2021-07-24 18:29:10 +0530

    .

--- src_files/main.cpp
+++ src_files/main.cpp
@@ -53,33 +53,11 @@ int main(int argc, char *argv[]) {
     GeneratorPool pool(getParam("-threads", 1));
     pool.run(getParam("-games", 10));
 #else
-#ifndef TUNING
-
-
     if (argc == 1) {
         uci::mainloop(false);
     } else if (argc > 1 && strcmp(argv[1], "bench") == 0) {
         uci::mainloop(true);
     }
-#else
-using namespace tuning;
-
-    bb::init();
-    psqt_init();
-
-    load_weights();
-
-    load_positions("../resources/other/E12.33-1M-D12-Resolved.book", 10000000);
-    load_positions("../resources/other/E12.41-1M-D12-Resolved.book", 10000000);
-    load_positions("../resources/other/E12.52-1M-D12-Resolved.book", 10000000);
-    load_positions("../resources/other/E12.46FRC-1250k-D12-1s-Resolved.book", 10000000);
-    load_positions("../resources/other/E13.04-Filtered.fens", 14000000);
-    double K = compute_K(2.48617, 100, 1e-6);
-    for(int i = 0; i < 10; i++){
-        train(50, K, 0.001 * sqrt(positions.size()));
-        display_params();
-    }
-#endif
 #endif
     return 0;
 }
