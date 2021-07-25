aa3293f138649c012297d438f306828734ae1ef9
Kim Kåhre <kim.kahre@helsinki.fi>
2021-07-26 01:53:00 +0300

    test
    bench: 4542157

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -443,7 +443,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
         // if a qsearch on the current position is far below beta at low depth, we can fail soft.
         // **********************************************************************************************************
         if (depth <= 3 && staticEval + RAZOR_MARGIN < beta) {
-            score = qSearch(b, alpha, beta, ply, td, false, false);
+            score = qSearch(b, alpha, beta, ply, td, false);
             if (score < beta) {
                 return score;
             } else if (depth == 1)
