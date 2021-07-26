3cd3c879d67181c388d6c5e60d91d620d8326b85
Kim Kahre <kimkahre@outlook.com>
2021-07-26 12:08:36 +0300

    test
    bench: 4610918

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -669,7 +669,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
         } else if (depth < 8) {
             if (legalMoves == 1 && hashMove && en.type == CUT_NODE && getSquareFrom(hashMove) == getSquareTo(sd->killer[!b->getActivePlayer()][ply + 1][0]))
                 extension = 1;
-            if (legalMoves == 1 && hashMove && en.type == CUT_NODE && getSquareTo(hashMove) == getSquareTo(b->getPreviousMove()))
+            if (legalMoves == 1 && hashMove && en.type == CUT_NODE && getSquareTo(hashMove) == getSquareTo(b->getPreviousMove()) && sd->eval[b->getActivePlayer()][ply] < alpha)
                 extension = 1;
         }
         // *********************************************************************************************************
