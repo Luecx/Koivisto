a2b819fcc1e1c7254cee05de6cf4ab14cd80625a
Kim Kahre <kimkahre@outlook.com>
2021-08-05 16:43:04 +0300

    test
    bench: 4426172

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -497,7 +497,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
     // this is based on other top engines.
     // **********************************************************************************************************
 
-    Score     betaCut = beta + FUTILITY_MARGIN;
+    Score     betaCut = beta + 90;
     if (!inCheck && !pv && depth > 4 && !skipMove && ownThreats
         && !(hashMove && en.depth >= depth - 3 && en.score < betaCut)) {
         generateNonQuietMoves(b, mv, hashMove, sd, ply);
