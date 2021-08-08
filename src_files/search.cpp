75e8ff1ca7db15778b8dd349c4b7e1e759d4e586
Kim Kahre <kimkahre@outlook.com>
2021-08-09 00:59:31 +0300

    test
    bench: 3945376

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -715,6 +715,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             if (lmr > MAX_PLY) {
                 lmr = 0;
             }
+            lmr -= isCapture(m);
             if (lmr > depth - 2) {
                 lmr = depth - 2;
             }
