004a78f534fdbd2033f832844cc6bc5ee8bb6b7d
Kim Kahre <kimkahre@outlook.com>
2021-08-14 17:42:29 +0300

    test
    bench: 4373116

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -515,7 +515,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             Score qScore = -qSearch(b, -betaCut, -betaCut + 1, ply + 1, td);
 
             if (qScore >= betaCut)
-                qScore = -pvSearch(b, -betaCut, -betaCut + 1, depth - 4, ply + 1, td, 0, behindNMP);
+                qScore = -pvSearch(b, -betaCut, -betaCut + 1, depth - 4, ply + 1, td, 0, !b->getActivePlayer());
 
             b->undoMove();
 
@@ -652,7 +652,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             && (en.type == CUT_NODE || en.type == PV_NODE) && en.depth >= depth - 3) {
 
             betaCut = en.score - SE_MARGIN_STATIC - depth * 2;
-            score   = pvSearch(b, betaCut - 1, betaCut, depth >> 1, ply, td, m, behindNMP);
+            score   = pvSearch(b, betaCut - 1, betaCut, depth >> 1, ply, td, m, b->getActivePlayer());
             if (score < betaCut) {
                 if (lmrFactor != nullptr) {
                     depth += *lmrFactor;
@@ -662,7 +662,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             } else if (score >= beta) {
                 return score;
             } else if (en.score >= beta) {
-                score = pvSearch(b, beta - 1, beta, (depth >> 1) + 3, ply, td, m, behindNMP);
+                score = pvSearch(b, beta - 1, beta, (depth >> 1) + 3, ply, td, m, b->getActivePlayer());
                 if (score >= beta)
                     return score;
             }
