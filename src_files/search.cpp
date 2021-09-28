8185096a4eed8347b5ac032a295d12371dd15e1a
Kim Kåhre <kim.kahre@helsinki.fi>
2021-09-28 03:21:19 +0300

    test2
    bench:  4101308

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -198,9 +198,9 @@ Move Search::bestMove(Board* b, Depth maxDepth, TimeManager* timeManager, int th
         if (d < 6) {
             s = this->pvSearch(&searchBoard, -MAX_MATE_SCORE, MAX_MATE_SCORE, d, 0, td, 0, 2);
         } else {
-            Score window = 5;
+            Score window = 10;
             Score alpha  = s;
-            Score beta   = s + 1;
+            Score beta   = s + 10;
             Depth sDepth = d;    // Idea of reducing depth on fail high from Houdini.
                                  // http://www.talkchess.com/forum3/viewtopic.php?t=45624.
             while (this->isTimeLeft()) {
