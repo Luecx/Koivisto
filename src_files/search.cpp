b7e43129cfc7abf8dbde2c5e67162e43f80855f7
Kim Kåhre <kim.kahre@helsinki.fi>
2021-10-13 19:49:32 +0300

    test
    bench: 4615253

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -738,18 +738,13 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             }
 
 
-            if (lmr && depth - lmr > 4 && b->getActivePlayer() == 1 - behindNMP) {
-                // reduced search.
-                score = -pvSearch(b, -alpha - 1, -alpha, depth - 3 * ONE_PLY - lmr + extension, ply + ONE_PLY,
-                                td, 0, lmr != 0 ? b->getActivePlayer() : behindNMP, &lmr);
-                if (score > alpha)
-                    score = -pvSearch(b, -alpha - 1, -alpha, depth - ONE_PLY - lmr + extension, ply + ONE_PLY,
-                                td, 0, lmr != 0 ? b->getActivePlayer() : behindNMP, &lmr);
-            } else {
-                // reduced search.
-                score = -pvSearch(b, -alpha - 1, -alpha, depth - ONE_PLY - lmr + extension, ply + ONE_PLY,
-                                td, 0, lmr != 0 ? b->getActivePlayer() : behindNMP, &lmr);
-            }
+            // reduced search.
+            score = -pvSearch(b, -alpha - 1, -alpha, depth - ONE_PLY - lmr + extension, ply + ONE_PLY,
+                        td, 0, lmr != 0 ? b->getActivePlayer() : behindNMP, &lmr);
+            if (!pv && score > alpha && lmr && lmr > 3)
+                score = -pvSearch(b, -alpha - 1, -alpha, depth - ONE_PLY - (lmr / 2) + extension, ply + ONE_PLY,
+                            td, 0, lmr != 0 ? b->getActivePlayer() : behindNMP, &lmr);
+
             // more kk reduction logic.
             if (pv)
                 sd->reduce = true;
