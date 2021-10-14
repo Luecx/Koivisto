69df1e13e971e2cabae2ea19aadc8f319d7158e1
Kim Kåhre <kim.kahre@helsinki.fi>
2021-10-14 23:28:01 +0300

    test
    bench: 4420989

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -473,7 +473,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             b->move_null();
             score =
                 -pvSearch(b, -beta, 1 - beta,
-                          depth - (depth / 4 + 3) * ONE_PLY
+                          depth - (depth / 4 + 4) * ONE_PLY
                               - (staticEval - beta < 300 ? (staticEval - beta) / FUTILITY_MARGIN : 3),
                           ply + ONE_PLY, td, 0, !b->getActivePlayer());
             b->undoMove_null();
