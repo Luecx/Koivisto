cc213486a6aca8f82633303b0af0e798cb1941aa
Kim Kåhre <kim.kahre@helsinki.fi>
2021-10-15 19:37:42 +0300

    test
    bench: 4527105

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -400,6 +400,15 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
         }
     }
 
+
+    // **********************************************************************************************************
+    // internal iterative deepening by Ed Schröder::
+    // http://talkchess.com/forum3/viewtopic.php?f=7&t=74769&sid=64085e3396554f0fba414404445b3120
+    // **********************************************************************************************************
+    if (depth >= 4 && !hashMove)
+        depth--;
+
+
     // **************************************************************************************************************
     // tablebase probing:
     // search the wdl table if we are not at the root and the root did not use the wdl table to sort
@@ -520,13 +529,6 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             }
         }
     }
-
-    // **********************************************************************************************************
-    // internal iterative deepening by Ed Schröder::
-    // http://talkchess.com/forum3/viewtopic.php?f=7&t=74769&sid=64085e3396554f0fba414404445b3120
-    // **********************************************************************************************************
-    if (depth >= 4 && !hashMove)
-        depth--;
 
     // **********************************************************************************************************
     // mate distance pruning:
