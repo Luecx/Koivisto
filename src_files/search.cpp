188875303cd52a9db5d9a0974cd8817d5f6b1238
Kim Kåhre <kim.kahre@helsinki.fi>
2021-10-21 15:17:55 +0300

    test
    bench: 4984623

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -699,6 +699,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             lmr = lmr - history / 150;
             lmr += !isImproving;
             lmr -= pv;
+            lmr -= abs(sd->eval[b->getActivePlayer()][ply] - alpha) < 25;
             if (!sd->targetReached) 
                 lmr++;
             if (sd->isKiller(m, ply, b->getActivePlayer()))
