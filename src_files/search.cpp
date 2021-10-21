0a0e10027471040d3f92e8084ccff151bc238b7d
Kim Kåhre <kim.kahre@helsinki.fi>
2021-10-21 14:04:59 +0300

    test
    bench: 5220831

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -661,7 +661,10 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             moveOrderer = {mv};
 
             m           = moveOrderer.next(0);
-        }
+        } else if (ply > 0 && pv && sameMove(hashMove, m) && getSquareTo(m) == getSquareTo(b->getPreviousMove()))
+            extension = 1;
+
+
         // *********************************************************************************************************
         // kk reductions:
         // we reduce more/less depending on which side we are currently looking at. The idea behind
