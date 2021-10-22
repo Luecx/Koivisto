2a46cc61ba926d4c7889a09dfba96a091970f878
Kim Kåhre <kim.kahre@helsinki.fi>
2021-10-22 22:09:44 +0300

    test
    bench: 4410582

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -938,12 +938,6 @@ Score Search::qSearch(Board* b, Score alpha, Score beta, Depth ply, ThreadData* 
         if (!b->isLegal(m))
             continue;
 
-        // if the move seems to be really good just return beta.
-        if (+see_piece_vals[(getPieceType(getCapturedPiece(m)))]
-                - see_piece_vals[getPieceType(getMovingPiece(m))] - 300 + stand_pat
-            > beta)
-            return beta;
-
         // *******************************************************************************************
         // static exchange evaluation pruning (see pruning):
         // if the depth is small enough and the static exchange evaluation for the given move is very
