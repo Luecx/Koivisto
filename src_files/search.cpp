bfd3379a685b6ef97e662bd5916963149535554c
Kim Kåhre <kim.kahre@helsinki.fi>
2021-10-25 23:18:36 +0300

    test2
    bench: 4059022

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -450,7 +450,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
         // will definetly be above beta and stop the search here and fail soft. Also reuse information
         // from eval to prevent pruning if the oponent has multiple threats.
         // **********************************************************************************************************
-        if (depth <= 7 && enemyThreats < 2 && staticEval >= beta + (depth + ONE_PLY - (isImproving + enemyThreats)) * FUTILITY_MARGIN
+        if (depth <= 7 && enemyThreats < 2 && staticEval >= beta + std::max(depth - (isImproving + enemyThreats), 0) * FUTILITY_MARGIN
             && staticEval < MIN_MATE_SCORE)
             return staticEval;
 
