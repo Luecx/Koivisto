b71f693719597ba72810048557a90c2b357e5d7f
Kim Kåhre <kim.kahre@helsinki.fi>
2021-10-28 13:21:49 +0300

    test
    bench: 3782086

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -729,7 +729,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
 
         // adjust the extension policy for checks. we could use the givesCheck value but it has not
         // been validated to work 100%
-        if (extension == 0 && depth > 3 && b->isInCheck(b->getActivePlayer()))
+        if (extension == 0 && depth > 5 && b->isInCheck(b->getActivePlayer()))
             extension = 1;
 
         mv->scoreMove(moveOrderer.counter - 1, depth + (staticEval < alpha));
