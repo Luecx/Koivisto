c69fcc58684ccd80ec597b437bdcf3b9e2c7dd36
Kim Kåhre <kim.kahre@helsinki.fi>
2021-11-05 02:28:06 +0200

    test
    bench: 3922057

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -698,6 +698,9 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
         if (legalMoves > 0 && depth > 2 && b->getActivePlayer() == behindNMP)
             lmr++;
 
+        if (legalMoves == 0 && !b->getActivePlayer() == behindNMP && isCapture(m) && getSquareTo(m) == getSquareTo(b->getPreviousMove()))
+            extension = 1;
+
         // depending on if lmr is used, we adjust the lmr score using history scores and kk-reductions
         // etc. Most conditions are standard and should be considered self explanatory.
         if (lmr) {
