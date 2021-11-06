ff3804ce5a36587ca4ba7c9477b0a31e7d82a57b
Kim Kåhre <kim.kahre@helsinki.fi>
2021-11-06 18:54:58 +0200

    test
    bench: 3755083

--- src_files/Board.cpp
+++ src_files/Board.cpp
@@ -25,7 +25,7 @@
 using namespace bb;
 
 float phaseValues[N_PIECE_TYPES] {
-    0, 1, 1, 2, 4, 0,
+    0, 3, 3, 5, 10, 0,
 };
 
 /**
@@ -1208,11 +1208,11 @@ template<Color side> U64 Board::getPinnedPieces(U64& pinners) {
 }
 
 Score Board::evaluate(){
-    float phase = (24.0f + phaseValues[5] - phaseValues[0] * bitCount(getPieceBB()[WHITE_PAWN] | getPieceBB()[BLACK_PAWN])
+    float phase = (64.0f + phaseValues[5] - phaseValues[0] * bitCount(getPieceBB()[WHITE_PAWN] | getPieceBB()[BLACK_PAWN])
          - phaseValues[1] * bitCount(getPieceBB()[WHITE_KNIGHT] | getPieceBB()[BLACK_KNIGHT])
          - phaseValues[2] * bitCount(getPieceBB()[WHITE_BISHOP] | getPieceBB()[BLACK_BISHOP])
          - phaseValues[3] * bitCount(getPieceBB()[WHITE_ROOK] | getPieceBB()[BLACK_ROOK])
          - phaseValues[4] * bitCount(getPieceBB()[WHITE_QUEEN] | getPieceBB()[BLACK_QUEEN]))
-         / 24.0f;
+         / 64.0f;
     return (2.0f - phase) * 0.8f * this->evaluator.evaluate(this->getActivePlayer());
 }
