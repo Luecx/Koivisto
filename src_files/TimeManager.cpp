cf32d9113cea7b18e959eef232c1e1dc47e01da8
Kim Kåhre <kim.kahre@helsinki.fi>
2021-11-11 03:52:12 +0200

    test
    bench: 3640469

--- src_files/TimeManager.cpp
+++ src_files/TimeManager.cpp
@@ -87,7 +87,7 @@ TimeManager::TimeManager(int white, int black, int whiteInc, int blackInc, int m
     upperTimeBound = board->getActivePlayer() == WHITE ? (int(white / division)*3 + whiteInc) - 25
                                                        : (int(black / division)*3 + blackInc) - 25;
 
-    timeToUse = upperTimeBound / 3;
+    timeToUse = upperTimeBound / 3 + board->getActivePlayer() == WHITE ? whiteInc / 3 : blackInc / 3;
 
     timeToUse = std::min(timeToUse, board->getActivePlayer() == WHITE ? white - 50 : black - 50);
     upperTimeBound = std::min(upperTimeBound, board->getActivePlayer() == WHITE ? white - 50 : black - 50);
