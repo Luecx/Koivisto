b3f3883f56bff1e4381adfca10cd3f9fb1c4bb0b
Kim Kåhre <kim.kahre@helsinki.fi>
2021-11-20 21:12:26 +0200

    test
    bench: 3455658

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -927,7 +927,7 @@ Score Search::qSearch(Board* b, Score alpha, Score beta, Depth ply, ThreadData* 
         alpha = bestScore;
 
     moveGen* mGen   = &td->generators[ply];
-    mGen->init(sd, b, ply, 0, 0, 0, Q_SEARCH + inCheck);
+    mGen->init(sd, b, ply, 0, b->getPreviousMove(), ply > 1 ? sd->playedMoves[ply - 2] : 0, Q_SEARCH + inCheck);
 
     // keping track of the best move for the transpositions
     Move        bestMove = 0;
