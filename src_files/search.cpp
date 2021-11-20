0de442fe3cfbe8d761927b201133e41b51d235be
Kim Kåhre <kim.kahre@helsinki.fi>
2021-11-20 17:29:04 +0200

    test
    bench: 3428875

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -702,7 +702,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
         // etc. Most conditions are standard and should be considered self explanatory.
         if (lmr) {
             int history = sd->getHistories(m, b->getActivePlayer(), b->getPreviousMove(), ply > 1 ? sd->playedMoves[ply - 2] : 0);
-            lmr = lmr - history / 150;
+            lmr = lmr - max(history, -300) / 150;
             lmr += !isImproving;
             lmr -= pv;
             if (!sd->targetReached) 
