1537b0a96d9bc9b756f65e519c5d1808e431603e
Kim Kåhre <kim.kahre@helsinki.fi>
2021-11-22 02:14:10 +0200

    test
    bench: 3330182

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -783,8 +783,9 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
                 table->put(zobrist, score, m, CUT_NODE, depth);
             }
             // also set this move as a killer move into the history
-            if (!isCapture(m))
+            if (!isCapture(m) && !isPromotion) {
                 sd->setKiller(m, ply, b->getActivePlayer());
+            }
 
             // update history scores
             mGen->updateHistory(depth + (staticEval < alpha));
