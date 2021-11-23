1cbc5efcaaa47621d889e780ad5fde90a655d08e
Kim Kåhre <kim.kahre@helsinki.fi>
2021-11-23 02:03:26 +0200

    test
    bench: 3475154

--- src_files/newmovegen.h
+++ src_files/newmovegen.h
@@ -52,8 +52,6 @@ enum {
 class moveGen {
     private:
 
-    int             stage;
-
     Move            quiets[MAX_QUIET]       = {0};
     Move            noisy[MAX_NOISY]        = {0};
     Move            searched[MAX_QUIET]     = {0};
@@ -80,6 +78,9 @@ class moveGen {
     int             m_mode;
 
     public: 
+
+    int             stage;
+
     void init(SearchData* sd, Board* b, Depth ply, Move hashMove, Move previous, Move followup, int mode, U64 checkerSq = 0);
     Move next();
     void addNoisy(Move m);
--- src_files/search.cpp
+++ src_files/search.cpp
@@ -584,7 +584,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
                 // **************************************************************************************************
                 if (mGen->shouldSkip())
                     continue;
-                if (depth <= 7 && quiets >= lmp[isImproving][depth]) {
+                if (depth <= 7 && quiets >= lmp[isImproving][depth] - (mGen->stage < GET_QUIET)) {
                     mGen->skip();
                 }
                 
