40a5b28f2a90dd05af4e6bd1ae5a203ad5cc92e4
Kim Kåhre <kim.kahre@helsinki.fi>
2021-11-25 00:51:39 +0200

    test
    bench: 3275246

--- src_files/History.cpp
+++ src_files/History.cpp
@@ -24,6 +24,12 @@ int SearchData::getHistories(Move m, Color side, Move previous, Move followup, S
     if (isCapture(m)) {
         return captureHistory[side][getSqToSqFromCombination(m)];
     } else {
+        if (threatSquare) {
+            return (2 * (followup != 0 ? fmh[getPieceTypeSqToCombination(followup)][side][getPieceTypeSqToCombination(m)] : 0)
+                   + 2 * cmh[getPieceTypeSqToCombination(previous)][side][getPieceTypeSqToCombination(m)]
+                   + 2 * th[side][threatSquare][getSqToSqFromCombination(m)]
+                   + 2 * th[side][threatSquare][getSqToSqFromCombination(m)]) / 4;
+        }
         return (2 * (followup != 0 ? fmh[getPieceTypeSqToCombination(followup)][side][getPieceTypeSqToCombination(m)] : 0)
                + 2 * cmh[getPieceTypeSqToCombination(previous)][side][getPieceTypeSqToCombination(m)]
                + 2 * th[side][threatSquare][getSqToSqFromCombination(m)]) / 3;
