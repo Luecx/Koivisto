23f0f27bb15d3835cfe4dd535e606410117e3920
Kim Kåhre <kim.kahre@helsinki.fi>
2021-12-16 05:55:25 +0200

    test
    bench: 3444372

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -765,10 +765,9 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
                 sd->reduce = true;
             if (ply == 0) {
                 sd->sideToReduce = b->getActivePlayer();
-            }
-
+            } 
             if (lmr && score > alpha)
-                score = -pvSearch(b, -alpha - 1, -alpha, depth - ONE_PLY + extension,
+                score = -pvSearch(b, -alpha - 1, -alpha, depth - ONE_PLY + extension + (lmrFactor != nullptr ? *lmrFactor : 0),
                                   ply + ONE_PLY, td, 0, behindNMP);    // re-search
             if (score > alpha && score < beta)
                 score = -pvSearch(b, -beta, -alpha, depth - ONE_PLY + extension, ply + ONE_PLY,
