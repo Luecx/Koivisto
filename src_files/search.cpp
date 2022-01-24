6e4937a24b1b880aad275b6023c346478824be89
Kim Kåhre <kim.kahre@helsinki.fi>
2022-01-24 23:20:33 +0200

    test
    bench:  3313320

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -760,6 +760,10 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             // reduced search.
             score = -pvSearch(b, -alpha - 1, -alpha, depth - ONE_PLY - lmr + extension, ply + ONE_PLY,
                               td, 0, lmr != 0 ? b->getActivePlayer() : behindNMP, &lmr);
+
+            if (score > alpha && !pv)
+                bestMove = m;
+
             // more kk reduction logic.
             if (pv)
                 sd->reduce = true;
