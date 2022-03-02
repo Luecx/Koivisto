2ffaf3e91d924d8faa18c1de1430da90b3f57d8e
Kim Kåhre <kim.kahre@helsinki.fi>
2022-03-03 01:38:09 +0200

    test
    bench: 3647046

--- src_files/TimeManager.cpp
+++ src_files/TimeManager.cpp
@@ -50,10 +50,10 @@ void TimeManager::setMatchTimeLimit (U64 time, U64 inc, int moves_to_go) {
     UCI_ASSERT(moves_to_go >= 0);
     
     constexpr U64 overhead = 50;
-    const double  division = moves_to_go + 1;
+    const double  division = 2;
     
-    U64 upperTimeBound = (int(time / division) * 3 + std::min(time * 0.9 + inc, inc * 3.0) - 25);
-    U64 timeToUse      = upperTimeBound / 3;
+    U64 upperTimeBound = time / 2;
+    U64 timeToUse      = 2 * inc + time / 10;
     
     timeToUse          = std::min(time - inc, timeToUse);
     upperTimeBound     = std::min(time - inc, upperTimeBound);
@@ -106,6 +106,8 @@ bool TimeManager::rootTimeLeft(int score) const {
     // stop the search if requested
     if (force_stop)
         return false;
+
+    score = 100 - std::min(score, 85);
     
     int elapsed = elapsedTime();
     
@@ -120,7 +122,7 @@ bool TimeManager::rootTimeLeft(int score) const {
     // 100, we half the time to use. If it's lower than 30, it reaches a maximum of 1.4 times the
     // original time to use.
     if(    match_time_limit.enabled
-        && match_time_limit.time_to_use * 50.0 / std::max(score, 30) < elapsed)
+        && match_time_limit.time_to_use * score / 100 < elapsed)
         return false;
     
     return true;
