7d1a0976376bd24458c43f83de6f6dc813e01ce7
Kim Kåhre <kim.kahre@helsinki.fi>
2022-03-16 23:29:54 +0200

    test
    bench: 4004327

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -708,6 +708,15 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             if (legalMoves == 0) {
                 sd->reduce = true;
             }
+        } else if (depth < 8
+               && !skipMove
+               && !inCheck
+               && !isCapture(hashMove)
+               &&  sameMove(m, hashMove)
+               &&  ply >  0
+               &&  sd->eval[b->getActivePlayer()][ply] < alpha - 50
+               &&  en.type == CUT_NODE) {
+            extension = 1;
         }
 
         U64   nodeCount = td->nodes;
