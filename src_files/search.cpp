9ae34f6fe7deb6f8b11e6585ca55b406bd8205b1
Kim Kåhre <kim.kahre@helsinki.fi>
2022-04-21 01:44:39 +0300

    test
    bench: 4594067

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -685,7 +685,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             // ***************************************************************************************
             if (moveDepth <= 5 + quiet * 3
                 && (getCapturedPieceType(m)) < (getMovingPieceType(m))
-                && (isCapture(m) ? mGen->lastSee : b->staticExchangeEvaluation(m)) <= (quiet ? -40 * moveDepth : -80 * depth))
+                && (isCapture(m) ? mGen->lastSee : b->staticExchangeEvaluation(m)) <= (quiet ? -40 * moveDepth : -120 * depth))
                 continue;
         }
 
