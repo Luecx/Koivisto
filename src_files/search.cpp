60497782f521b58b1cde3ad56e518405256e7b13
Finn Eggers <web@finneggers.de>
2022-05-20 14:25:50 +0200

    Auto generated
    bench:4587553

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -535,7 +535,8 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
         // assume that we could achieve beta, so we can return early. Don't do nmp when the oponent
         // has threats or the position or we don't have non-pawn material.
         // *******************************************************************************************
-        if (staticEval >= beta + (5 > depth ? 30 : 0) && !(depth < 5 && enemyThreats > 0)
+        if (    staticEval >= beta + (5 > depth ? 30 : 0)
+            && !(depth < 5 && enemyThreats > 1)
             && !hasOnlyPawns(b, b->getActivePlayer())) {
             b->move_null();
             score =
