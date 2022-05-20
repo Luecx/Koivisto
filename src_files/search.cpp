f713f52f557176a7f5a27ac9deb113224338162e
Finn Eggers <web@finneggers.de>
2022-05-20 12:47:56 +0200

    Auto generated
    bench:4595595

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -521,7 +521,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
         // from eval to prevent pruning if the oponent has multiple threats.
         // *******************************************************************************************
         if (   depth        <= 7
-            && enemyThreats <  2
+            && enemyThreats <  3
             && staticEval   >= beta + (depth - (isImproving && !enemyThreats)) * FUTILITY_MARGIN
             && staticEval   <  MIN_MATE_SCORE)
             return staticEval;
