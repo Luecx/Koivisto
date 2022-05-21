2d2fbf3fc21a9a4dd1e8065391383ae649386b1c
Finn Eggers <web@finneggers.de>
2022-05-21 22:19:04 +0200

    Auto generated
    bench:5246866

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -507,7 +507,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
         // razoring:
         // if a qsearch on the current position is far below beta at low depth, we can fail soft.
         // **********************************************************************************************************
-        if (depth <= 3 && staticEval + RAZOR_MARGIN < beta) {
+        if (depth <= 3 && staticEval + RAZOR_MARGIN * depth < beta) {
             score = qSearch(b, alpha, beta, ply, td);
             if (score < beta) {
                 return score;
