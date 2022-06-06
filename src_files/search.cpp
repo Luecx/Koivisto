d47b02f8d3c663308f317d2b0ff3a9361e0a8092
Kim Kåhre <kim.kahre@helsinki.fi>
2022-06-06 09:20:02 +0300

    test
    bench: 7375817

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -804,7 +804,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
         if (extension == 0 && depth > 4 && b->isInCheck(b->getActivePlayer()))
             extension = 1;
 
-        if (sameMove(hashMove, m) && !pv && en.type > ALL_NODE)
+        if (sameMove(hashMove, m) && en.type > ALL_NODE)
             extension = 1;
 
         // principal variation search recursion.
