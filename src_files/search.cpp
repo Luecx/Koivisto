4f616a949bff51c05afb33c90ba652c9e3a83730
Luecx <web@finneggers.de>
2022-09-23 10:46:37 +0200

    Auto generated
    bench:3977556

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -548,8 +548,8 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
             b->move_null();
             score =
                 -pvSearch(b, -beta, 1 - beta,
-                          depth - (depth / 4 + 3) * ONE_PLY
-                              - (staticEval - beta < 300 ? (staticEval - beta) / FUTILITY_MARGIN : 3),
+                          depth - (depth / 4 + 3)
+                                - std::min((staticEval - beta) / FUTILITY_MARGIN, 2),
                           ply + ONE_PLY, td, 0, !b->getActivePlayer());
             b->undoMove_null();
             if (score >= beta) {
