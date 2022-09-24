3becf139180a3adbd58496b4d4020358c6c9f0f3
Luecx <web@finneggers.de>
2022-09-25 00:34:06 +0200

    Auto generated
    bench:3983323

--- src_files/eval.cpp
+++ src_files/eval.cpp
@@ -259,23 +259,36 @@ void nn::Evaluator::setPieceOnSquareAccumulator(bb::Color side, bb::PieceType pi
                                                 bb::Square kingSquare) {
     const int  idx = index(pieceType, pieceColor, square, side, kingSquare);
 
-    const auto wgt = (avx_register_type_16*) (inputWeights[idx]);
-    const auto sum = (avx_register_type_16*) (history.back().summation[side]);
-    if constexpr (value) {
-        for (int i = 0; i < HIDDEN_SIZE / STRIDE_16_BIT / 4; i++) {
-            sum[i * 4 + 0] = avx_add_epi16(sum[i * 4 + 0], wgt[i * 4 + 0]);
-            sum[i * 4 + 1] = avx_add_epi16(sum[i * 4 + 1], wgt[i * 4 + 1]);
-            sum[i * 4 + 2] = avx_add_epi16(sum[i * 4 + 2], wgt[i * 4 + 2]);
-            sum[i * 4 + 3] = avx_add_epi16(sum[i * 4 + 3], wgt[i * 4 + 3]);
+    
+    const auto wgt = inputWeights[idx];
+    const auto sum = history.back().summation[side];
+    if constexpr (value){
+        for(int i = 0; i < HIDDEN_SIZE; i++){
+            sum[i] += wgt[i];
         }
-    } else {
-        for (int i = 0; i < HIDDEN_SIZE / STRIDE_16_BIT / 4; i++) {
-            sum[i * 4 + 0] = avx_sub_epi16(sum[i * 4 + 0], wgt[i * 4 + 0]);
-            sum[i * 4 + 1] = avx_sub_epi16(sum[i * 4 + 1], wgt[i * 4 + 1]);
-            sum[i * 4 + 2] = avx_sub_epi16(sum[i * 4 + 2], wgt[i * 4 + 2]);
-            sum[i * 4 + 3] = avx_sub_epi16(sum[i * 4 + 3], wgt[i * 4 + 3]);
+    }else{
+        for(int i = 0; i < HIDDEN_SIZE; i++){
+            sum[i] -= wgt[i];
         }
     }
+    
+//        const auto wgt = (avx_register_type_16*) (inputWeights[idx]);
+//        const auto sum = (avx_register_type_16*) (history.back().summation[side]);
+//    if constexpr (value) {
+//        for (int i = 0; i < HIDDEN_SIZE / STRIDE_16_BIT / 4; i++) {
+//            sum[i * 4 + 0] = avx_add_epi16(sum[i * 4 + 0], wgt[i * 4 + 0]);
+//            sum[i * 4 + 1] = avx_add_epi16(sum[i * 4 + 1], wgt[i * 4 + 1]);
+//            sum[i * 4 + 2] = avx_add_epi16(sum[i * 4 + 2], wgt[i * 4 + 2]);
+//            sum[i * 4 + 3] = avx_add_epi16(sum[i * 4 + 3], wgt[i * 4 + 3]);
+//        }
+//    } else {
+//        for (int i = 0; i < HIDDEN_SIZE / STRIDE_16_BIT / 4; i++) {
+//            sum[i * 4 + 0] = avx_sub_epi16(sum[i * 4 + 0], wgt[i * 4 + 0]);
+//            sum[i * 4 + 1] = avx_sub_epi16(sum[i * 4 + 1], wgt[i * 4 + 1]);
+//            sum[i * 4 + 2] = avx_sub_epi16(sum[i * 4 + 2], wgt[i * 4 + 2]);
+//            sum[i * 4 + 3] = avx_sub_epi16(sum[i * 4 + 3], wgt[i * 4 + 3]);
+//        }
+//    }
     
 }
 
