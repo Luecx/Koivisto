42886e209ceb266acaa78bd7c44d56a43b7d75d4
Luecx <web@finneggers.de>
2023-01-14 13:48:54 +0100

    Auto generated
    bench:4114931

--- src_files/board.cpp
+++ src_files/board.cpp
@@ -1430,7 +1430,7 @@ template<Color side> U64 Board::getPinnedPieces(U64& pinners) const {
 }
 
 Score Board::evaluate(){
-    constexpr float evaluation_mg_scalar = 1.5;
+    constexpr float evaluation_mg_scalar = 1.35;
     constexpr float evaluation_eg_scalar = 1.25;
 
     constexpr float phase_sum = 39.6684;
