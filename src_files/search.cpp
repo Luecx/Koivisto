44462c5721170421ea0f01c3fc3c61c757f9c101
Kim Kåhre <kim.kahre@helsinki.fi>
2023-02-09 12:51:55 +0200

    test
    bench: 3532895

--- src_files/search.cpp
+++ src_files/search.cpp
@@ -997,7 +997,7 @@ Score Search::pvSearch(Board* b, Score alpha, Score beta, Depth depth, Depth ply
                 sd->setKiller(m, ply, b->getActivePlayer());
 
             // update history scores
-            mGen->updateHistory(depth + (staticEval < alpha));
+            mGen->updateHistory(depth + (staticEval < alpha) + (en.type >= ALL_NODE && en.key == TT_KEY(zob)));
 
             return highestScore;
         }
