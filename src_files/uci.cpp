7f42f55222d5954f87b4054bcfc042e4b905ad52
altarchess <57199282+altarchess@users.noreply.github.com>
2024-07-24 22:30:43 +0300

    Update uci.cpp
    bench: 366925

--- src_files/uci.cpp
+++ src_files/uci.cpp
@@ -100,7 +100,7 @@ void uci::mainloop(int argc, char* argv[]) {
                                     << ", " << nn::hiddenWeights[0][i]
                                     << ", " << -5000
                                     << ", " <<  5000
-                                    << ", " <<  40
+                                    << ", " <<  10
                                     << ", " <<  0.0004
                                     << std::endl;
     }
