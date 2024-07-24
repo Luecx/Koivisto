1fcb325c2e3996c2009a635dab320537da710639
altarchess <57199282+altarchess@users.noreply.github.com>
2024-07-24 16:14:46 +0300

    Update uci.cpp
    bench: 366925

--- src_files/uci.cpp
+++ src_files/uci.cpp
@@ -100,7 +100,7 @@ void uci::mainloop(int argc, char* argv[]) {
                                     << ", " << nn::hiddenWeights[0][i]
                                     << ", " << -5000
                                     << ", " <<  5000
-                                    << ", " <<  std::max(std::abs(nn::hiddenWeights[0][i])/10, 1)
+                                    << ", " <<  std::max(std::abs(nn::hiddenWeights[0][i])/5, 1)
                                     << ", " <<  0.0004
                                     << std::endl;
     }
